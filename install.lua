6cc73048dbe5d0fce3a13f158d3a9c0a60bbaa14
RubenHetKonijn <64010592+RubenHetKonijn@users.noreply.github.com>
2020-08-08 17:01:37 +0200

    Update install.lua

--- install.lua
+++ install.lua
@@ -1,3 +1,4 @@
 shell.run("wget https://raw.githubusercontent.com/omegacoustics/livefloppy/master/startup.lua /startup.lua")
 shell.run("wget https://raw.githubusercontent.com/omegacoustics/livefloppy/master/livefloppy/logo.txt /livefloppy/logo.lua")
 shell.run("wget https://raw.githubusercontent.com/omegacoustics/livefloppy/master/livefloppy/startup.lua /livefloppy/startup.lua")
+reboot
