5eeaa1e3ec13fb0c925b38768f98fd46d92bb05d
RubenHetKonijn <64010592+RubenHetKonijn@users.noreply.github.com>
2020-08-08 16:53:54 +0200

    Create install.lua

---
+++ install.lua
@@ -0,0 +1,3 @@
+shell.run("wget https://raw.githubusercontent.com/omegacoustics/livefloppy/master/startup.lua /startup.lua")
+shell.run("wget https://raw.githubusercontent.com/omegacoustics/livefloppy/master/livefloppy/logo.txt /livefloppy/logo.lua")
+shell.run("wget https://raw.githubusercontent.com/omegacoustics/livefloppy/master/livefloppy/startup.lua /livefloppy/startup.lua")
