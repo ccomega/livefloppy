bab2e4dec27217c8da25015029d90964eb757bf8
RubenHetKonijn <64010592+RubenHetKonijn@users.noreply.github.com>
2020-08-11 12:25:41 +0200

    Update install.lua

--- install.lua
+++ install.lua
@@ -1,4 +1,4 @@
 shell.run("wget https://raw.githubusercontent.com/ccomega/livefloppy/master/startup.lua /startup.lua")
 shell.run("wget https://raw.githubusercontent.com/ccomega/livefloppy/master/livefloppy/logo.txt /livefloppy/logo.lua")
 shell.run("wget https://raw.githubusercontent.com/ccomega/livefloppy/master/livefloppy/startup.lua /livefloppy/startup.lua")
-os.reboot
+os.reboot()
